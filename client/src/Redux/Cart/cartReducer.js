bc71cc7f241d7ed9845e5e85eee251696830b04f
Divya Chhabra <chhabra.divya1989@gmail.com>
2020-04-21 17:31:10 +0530

    Cart Page CRUD and Persist State implemented

--- client/src/Redux/Cart/cartReducer.js
+++ client/src/Redux/Cart/cartReducer.js
@@ -1,4 +1,4 @@
-import { ADD_TO_CART } from "./types";
+import { ADD_TO_CART, ADD_TO_QUANTITY, SUBTRACT_FROM_QUANTITY, DELETE_FROM_CART } from "./types";
 
 const INITIAL_STATE = {
     cart: [],
@@ -8,15 +8,54 @@ const INITIAL_STATE = {
 
 export const cartReducer = (state = INITIAL_STATE, action) => {
   let stateCopy = JSON.parse(JSON.stringify(state));
-  console.log(stateCopy, action.payload, action.type);
+  
+  let itemid;
   
  switch (action.type) {
    case ADD_TO_CART:  
                       console.log("ADD TO CART", action.type, action.payload);
-                      stateCopy.total+=action.payload.productprice * action.payload.quantity;
-                      stateCopy.cart.push(action.payload);
+            
+                      const existingitem = stateCopy.cart.find((cartitem)=>cartitem.productid==action.payload.productid);
+                      itemid = stateCopy.cart.indexOf(existingitem);
+                      console.log(existingitem);
+                      if(existingitem && 
+                        ((action.payload.deliverydate===stateCopy.cart[itemid].deliverydate) &&
+                        (action.payload.selHour===stateCopy.cart[itemid].selHour) &&
+                        (action.payload.selMin===stateCopy.cart[itemid].selMin) && 
+                        (action.payload.AMPM===stateCopy.cart[itemid].AMPM))){
+                        stateCopy.total+= action.payload.quantity * stateCopy.cart[itemid].productprice;
+                        stateCopy.cart[itemid].quantity = Number(stateCopy.cart[itemid].quantity)+Number(action.payload.quantity);
+                      }
+                      else{
+                        stateCopy.total+=action.payload.productprice * action.payload.quantity;
+                        stateCopy.cart.push(action.payload);
+                      }
                       return stateCopy;
-                      
+                     
+
+case DELETE_FROM_CART:  
+                      console.log("DELETE FROM CART", action.type, action.payload);
+                      itemid = action.payload; 
+                      stateCopy.total-= stateCopy.cart[itemid].quantity * stateCopy.cart[itemid].productprice;
+                      stateCopy.cart.splice(itemid,1);
+                      return {...state, cart:stateCopy.cart, total:stateCopy.total}
+
+case ADD_TO_QUANTITY:
+                      console.log("ADD TO QUANTITY", action.type, action.payload);
+                      itemid = action.payload;
+                      stateCopy.total+=stateCopy.cart[itemid].productprice;
+                      stateCopy.cart[itemid].quantity = Number(stateCopy.cart[itemid].quantity)+1;
+                      return stateCopy;
+
+case SUBTRACT_FROM_QUANTITY:
+                    console.log("SUBTRACT FROM QUANTITY", action.type, action.payload);
+                    itemid = action.payload;
+                    if(stateCopy.cart[itemid].quantity>=2){
+                    stateCopy.total-=stateCopy.cart[itemid].productprice;
+                    stateCopy.cart[itemid].quantity = Number(stateCopy.cart[itemid].quantity)-1;
+                    }
+
+                    return stateCopy;          
    default:
      return state;
  }
